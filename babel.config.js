2f74910a65f6edff98ede2f21b600e4766c48658
unknown <henrique.dcti@gmail.com>
2020-08-20 18:15:02 -0300

    server issue fixed

--- babel.config.js
+++ babel.config.js
@@ -6,10 +6,14 @@ module.exports = {
   plugins: [
     ['module-resolver', {
       alias: {
+        "@": "./src",
         "@modules": "./src/modules",
         "@config": "./src/config",
         "@shared": "./src/shared",
       }
     }],
+    "babel-plugin-transform-typescript-metadata",
+    ["@babel/plugin-proposal-decorators", { "legacy": true }],
+    ["@babel/plugin-proposal-class-properties", { "loose": true }],
   ]
 }
--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,6 +1,5 @@
 import { Router } from 'express';
-
-import usersRouter from '@modules/users/infra/http/routes/users.routes';
+import usersRouter from '@modules/users/infra/http/routes/users.routes'
 
 const routes = Router();
 
