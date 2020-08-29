e1d703f34e8d3d602fad30f77e46706f66da0817
Henrique Ducati <henrique.dcti@gmail.com>
2020-08-28 23:36:18 -0300

    created fake hash provider

--- src/modules/users/services/CreateUserService.spec.ts
+++ src/modules/users/services/CreateUserService.spec.ts
@@ -1,3 +1,11 @@
-test('sum two number', () => {
-  expect(1 + 2).toBe(3);
+import CreateUserService from './CreateUserService';
+
+describe('CreateUser', () => {
+  it('should be able to create a new user', () => {
+    expect(1 + 2).toBe(3);
+  });
+
+  it('should not be able to create a account with an existing address', () => {
+    expect(1 + 2).toBe(3);
+  });
 });
