a9d65b6bd2453b1c25f0510f3cac7a3f0e5f16b8
Henrique Ducati <henrique.dcti@gmail.com>
2020-08-30 19:12:44 -0300

    added userToken repository

--- src/modules/users/repositories/IUserTokensRepository.ts
+++ src/modules/users/repositories/IUserTokensRepository.ts
@@ -1,7 +1,6 @@
 import UserToken from '../infra/typeorm/entities/UserToken';
-import User from '../infra/typeorm/entities/User';
 
 export default interface IUserTokensRepository {
-  generate(user_id: string): Promise<User>;
+  generate(user_id: string): Promise<UserToken>;
   findByToken(token: string): Promise<UserToken | undefined>;
 }
