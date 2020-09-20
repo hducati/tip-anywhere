ce5db0cf9a55f3994813edc4afc9bcd414aa115f
unknown <henrique.dcti@gmail.com>
2020-09-20 20:53:04 -0300

    added createUserService test

--- src/modules/users/services/CreateUserService.spec.ts
+++ src/modules/users/services/CreateUserService.spec.ts
@@ -1,9 +1,32 @@
+import FakeHashProvider from '../providers/HashProvider/fakes/FakeHashProvider';
+import FakeUsersRepository from '../repositories/fakes/FakeUserRepository';
+import CreateUserService from './CreateUserService';
+
+let fakeUsersRepository: FakeUsersRepository;
+let fakeHashProvider: FakeHashProvider;
+let createUser: CreateUserService;
+
 describe('CreateUser', () => {
-  it('should be able to create a new user', () => {
-    expect(1 + 2).toBe(3);
+  beforeEach(() => {
+    fakeUsersRepository = new FakeUsersRepository();
+    fakeHashProvider = new FakeHashProvider();
+    createUser = new CreateUserService(fakeUsersRepository, fakeHashProvider);
   });
 
-  it('should not be able to create a account with an existing address', () => {
-    expect(1 + 2).toBe(3);
+  it('should be able to create a new user', async () => {
+    const date = new Date();
+    const user = await createUser.execute({
+      name: 'Felipe Santos',
+      email: 'felipesantos@gmail.com',
+      password: '12357234',
+      birthday_date: date,
+      description: 'achei bem interessante',
+      phone_number: '123123213',
+      telegram: 'sdasdsadsa',
+      whatsapp: 'asdasda',
+      facebook: 'daswdsadaw',
+    });
+
+    expect(user).toHaveProperty('id');
   });
 });
