4f8e55ac4c9aa3aca473fd1a17b7504b5e8e027e
Henrique Ducati <henrique.dcti@gmail.com>
2020-09-21 21:12:40 -0300

    added multer for image upload

---
+++ src/shared/container/providers/StorageProvider/implementations/DiskStorageProvider.ts
@@ -0,0 +1,15 @@
+import fs from 'fs';
+import path from 'path';
+import IStorageProvider from '../models/IStorageProvider';
+
+class DiskStorageProvider implements IStorageProvider {
+  public async saveFile(file: string): Promise<string> {
+    await fs.promises.rename(
+      path.resolve();
+    );
+  }
+
+  public async deleteFile(file: string): Promise<void> {}
+}
+
+export default DiskStorageProvider;
