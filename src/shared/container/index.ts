25cdd79313b3aacb4ab71d3dc55de95f98b335b2
unknown <henrique.dcti@gmail.com>
2020-10-25 16:03:04 -0300

    added tips container

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -9,6 +9,9 @@ import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepo
 import IUserTokensRepository from '@modules/users/repositories/IUserTokensRepository';
 import UserTokensRepository from '@modules/users/infra/typeorm/repositories/UserTokensRepository';
 
+import ITipsRepository from '@modules/tips/repositories/ITipsRepository';
+import TipsRepository from '@modules/tips/infra/typeorm/repositories/TipsRepository';
+
 container.registerSingleton<IUsersRepository>(
   'UsersRepository',
   UsersRepository,
@@ -18,3 +21,5 @@ container.registerSingleton<IUserTokensRepository>(
   'UserTokensRepository',
   UserTokensRepository,
 );
+
+container.registerSingleton<ITipsRepository>('TipsRepository', TipsRepository);
