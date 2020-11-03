2cb380765a8d4474a740f9bf319a41c8d577f4d2
unknown <henrique.dcti@gmail.com>
2020-11-03 19:27:58 -0300

    added follow and unfollow interface

--- src/modules/follows/repositories/IFollowsRepository.ts
+++ src/modules/follows/repositories/IFollowsRepository.ts
@@ -3,8 +3,10 @@ import Follow from '../infra/typeorm/entities/Follow';
 
 export default interface IFollowsRepository {
   findById(id: string): Promise<Follow | undefined>;
-  findFollowedUsers(id: string): Promise<Follow | undefined>;
-  findFollowers(id: string): Promise<Follow | undefined>;
+  findFollowedUsers(followed_user_id: string): Promise<Follow | undefined>;
+  findFollowers(follower_user_id: string): Promise<Follow | undefined>;
+  unfollow(id: string): Promise<Follow | undefined>;
+  follow(id: string): Promise<Follow | undefined>;
   create(data: ICreateFollowDTO): Promise<Follow>;
   save(follow: Follow): Promise<Follow>;
 }
