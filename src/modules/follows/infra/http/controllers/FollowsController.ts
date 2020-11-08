71e91dd857b211440af4926a81cdd7b90cb5cd2f
unknown <henrique.dcti@gmail.com>
2020-11-08 16:22:45 -0300

    added update function - follows controller

--- src/modules/follows/infra/http/controllers/FollowsController.ts
+++ src/modules/follows/infra/http/controllers/FollowsController.ts
@@ -2,6 +2,7 @@ import CreateFollowService from '@modules/follows/services/CreateFollowService';
 import UpdateFollowService from '@modules/follows/services/UpdateFollowService';
 import { container } from 'tsyringe';
 import { Request, Response } from 'express';
+import { classToClass } from 'class-transformer';
 
 export default class FollowsConstroller {
   public async create(request: Request, response: Response): Promise<Response> {
@@ -14,5 +15,17 @@ export default class FollowsConstroller {
     const follow = createFollow.execute({ follower_user_id, followed_user_id });
 
     return response.json(follow);
+  }
+
+  public async update(request: Request, response: Response): Promise<Response> {
+    const follower_user_id = request.user.id;
+
+    const { id } = request.body;
+
+    const updateFollow = container.resolve(UpdateFollowService);
+
+    const follow = updateFollow.execute({ id, follower_user_id });
+
+    return response.json(classToClass(follow));
   }
 }
