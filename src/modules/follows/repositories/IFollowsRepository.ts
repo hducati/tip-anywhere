1c9200a78d89f4d7b737b0575f498ddbabbf2a58
unknown <henrique.dcti@gmail.com>
2020-11-08 16:37:23 -0300

    changed follows repository

--- src/modules/follows/repositories/IFollowsRepository.ts
+++ src/modules/follows/repositories/IFollowsRepository.ts
@@ -3,8 +3,8 @@ import Follow from '../infra/typeorm/entities/Follow';
 
 export default interface IFollowsRepository {
   findById(id: string): Promise<Follow | undefined>;
-  findFollowedUsers(followed_user_id: string): Promise<Follow[] | undefined>;
-  findFollowers(follower_user_id: string): Promise<Follow[] | undefined>;
+  findFollowedUsers(followed_user_id: string): Promise<[Follow[], number]>;
+  findFollowers(follower_user_id: string): Promise<[Follow[], number]>;
   findUsers(
     followed_user_id: string,
     follower_user_id: string,
