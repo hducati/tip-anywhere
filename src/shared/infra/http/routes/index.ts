51b2d3965b68abdcca372f80dd1db862fd7b5551
unknown <henrique.dcti@gmail.com>
2020-11-08 20:05:22 -0300

    added follow router

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -2,11 +2,13 @@ import { Router } from 'express';
 import usersRouter from '@modules/users/infra/http/routes/users.routes';
 import sessionsRouter from '@modules/users/infra/http/routes/sessions.routes';
 import tipsRouter from '@modules/tips/infra/http/routes/tips.routes';
+import followsRouter from '@modules/follows/infra/http/routes/follows.routes';
 
 const routes = Router();
 
 routes.use('/users', usersRouter);
 routes.use('/sessions', sessionsRouter);
 routes.use('/tips', tipsRouter);
+routes.use('/follows', followsRouter);
 
 export default routes;
