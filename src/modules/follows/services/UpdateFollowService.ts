a92b58852ef61a89faf73c19fe83b342be43adc3
unknown <henrique.dcti@gmail.com>
2020-11-08 16:06:22 -0300

    created update follow service

---
+++ src/modules/follows/services/UpdateFollowService.ts
@@ -0,0 +1,39 @@
+import { injectable, inject } from 'tsyringe';
+import AppError from '@shared/errors/AppError';
+import IFollowsRepository from '../repositories/IFollowsRepository';
+import Follow from '../infra/typeorm/entities/Follow';
+
+interface IRequest {
+  id: string;
+  follower_user_id: string;
+}
+
+@injectable()
+export default class UpdateFollowService {
+  constructor(
+    @inject('FollowsRepository')
+    private followsRepository: IFollowsRepository,
+  ) {}
+
+  public async execute({ id, follower_user_id }: IRequest): Promise<Follow> {
+    const follow = await this.followsRepository.findById(id);
+
+    if (!follow) {
+      throw new AppError('Follow provided does not exist');
+    }
+
+    if (follow.follower_user_id !== follower_user_id) {
+      throw new AppError(
+        'Provider user tried to update a follow not created by himself',
+      );
+    }
+
+    if (follow.is_following === true) {
+      follow.is_following = false;
+    } else {
+      follow.is_following = true;
+    }
+
+    return this.followsRepository.save(follow);
+  }
+}
