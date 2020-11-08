3c143368a9677a5008fc415185f48f3b4c62f313
unknown <henrique.dcti@gmail.com>
2020-11-08 17:15:51 -0300

    created follows routes

---
+++ src/modules/follows/infra/http/routes/follows.routes.ts
@@ -0,0 +1,33 @@
+import { Router } from 'express';
+import { Joi, celebrate, Segments } from 'celebrate';
+
+import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
+import FollowsController from '../controllers/FollowsController';
+
+const followsRouter = Router();
+const followsController = new FollowsController();
+
+followsRouter.use(ensureAuthenticated);
+followsRouter.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      followed_user_id: Joi.string().required(),
+    },
+  }),
+
+  followsController.create,
+);
+
+followsRouter.put(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      id: Joi.string().required(),
+    },
+  }),
+
+  followsController.update,
+);
+
+export default followsRouter;
