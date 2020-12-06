76dd7dbf3eca62e9c6bafc644bf0045cf3089e68
Henrique Ducati <henrique.dcti@gmail.com>
2020-12-06 14:50:28 -0300

    Change 'findFollows' params to 'user_id'

--- src/modules/follows/repositories/IFollowsRepository.ts
+++ src/modules/follows/repositories/IFollowsRepository.ts
@@ -3,7 +3,7 @@ import Follow from '../infra/typeorm/entities/Follow';
 
 export default interface IFollowsRepository {
   findById(id: string): Promise<Follow | undefined>;
-  findFollows(followed_user_id: string): Promise<[Follow[], number]>;
+  findFollows(user_id: string): Promise<[Follow[], number]>;
   create(data: ICreateFollowDTO): Promise<Follow>;
   save(follow: Follow): Promise<Follow>;
 }
