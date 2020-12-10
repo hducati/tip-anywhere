f579f423cd2efce6083e48ac6b8dd4757939ad49
Henrique Ducati <henrique.dcti@gmail.com>
2020-12-10 15:40:29 -0300

    Fix 'Following' and 'Followers' routes

--- src/modules/follows/infra/http/routes/user_follows.routes.ts
+++ src/modules/follows/infra/http/routes/user_follows.routes.ts
@@ -3,13 +3,9 @@ import { Joi, celebrate, Segments } from 'celebrate';
 
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 import FollowsController from '../controllers/FollowsController';
-import FollowingController from '../controllers/FollowingController';
-import FollowersController from '../controllers/FollowersController';
 
 const followsRouter = Router();
 const followsController = new FollowsController();
-const followingController = new FollowingController();
-const followersController = new FollowersController();
 
 followsRouter.use(ensureAuthenticated);
 followsRouter.post(
@@ -34,7 +30,7 @@ followsRouter.put(
   followsController.update,
 );
 
-followsRouter.get('/following', followingController.index);
-followsRouter.get('/followers', followersController.index);
+followsRouter.get('/following', followsController.index);
+followsRouter.get('/followers', followsController.index);
 
 export default followsRouter;
